fa3dd9e5c4e1ff15f6c309f33931fbc8cfe86262
inaciojr48 <54165291+inaciojr48@users.noreply.github.com>
2021-12-14 01:32:33 +0200

    Task 2 update an map intent

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -39,7 +39,10 @@ public class InfoActivity extends AppCompatActivity {
         Uri uriAdress= Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW,uriAdress);
         mapIntent.setPackage("com.google.android.apps.maps");
+
+        if(mapIntent.resolveActivity(getPackageManager()) !=null){
         startActivity(mapIntent);
+        }
         }
 
     // ***
